5fb85b227eb504bcb1de0c8668b88d343ef02d20
Nahid <nahidthedeveloper@gmail.com>
2024-11-20 19:08:40 +0600

    nuxt ui fix

--- components/deleteModal.vue
+++ components/deleteModal.vue
@@ -12,11 +12,15 @@ defineProps({
 
     <UModal v-model="isOpen" :ui="{ width: 'w-[340px]' }">
       <div class="p-4 h-44 relative">
-        <UButton @click="isOpen = false" icon="gridicons:cross" class="absolute text-2xl top-4 right-4" color="blue" />
+        <UButton @click="isOpen = false" icon="gridicons:cross"
+          class="absolute bg-transparent text-2xl top-4 right-4" />
+
         <div class="flex justify-center mb-2">
           <UIcon name="material-symbols:delete-outline" class="w-14 h-14" />
         </div>
-        <h1 class="text-center mb-5">Are you sure to delete this user? id: {{ id }}</h1>
+        <h1 class="text-center mb-5">
+          Are you sure to delete this user? id: {{ id }}
+        </h1>
 
         <div class="flex gap-4 justify-center">
           <UButton @click="isOpen = false" color="blue">Cancel</UButton>
